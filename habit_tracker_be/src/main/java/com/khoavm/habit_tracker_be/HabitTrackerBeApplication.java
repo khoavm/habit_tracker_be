e91095e50161468f5e6d301303861bacc9b915d9
Vũ Minh Khoa <khoavm@MacBook-Pro-cua-Vu.local>
2024-03-13 01:20:16 +0700

    initial project

---
+++ habit_tracker_be/src/main/java/com/khoavm/habit_tracker_be/HabitTrackerBeApplication.java
@@ -0,0 +1,13 @@
+package com.khoavm.habit_tracker_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitTrackerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitTrackerBeApplication.class, args);
+	}
+
+}
---
+++ habit_tracker_be/src/test/java/com/khoavm/habit_tracker_be/HabitTrackerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khoavm.habit_tracker_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitTrackerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
